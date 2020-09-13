75a0f93e3055c3890200bea66c612e8053347f91
rjulian <richard@rjulian.net>
2020-09-13 14:46:17 -0700

    Semi-automated multi account push

--- source/reflex_aws_cloudwatch_logs_unencrypted.py
+++ source/reflex_aws_cloudwatch_logs_unencrypted.py
@@ -4,7 +4,7 @@ import json
 import os
 
 import boto3
-from reflex_core import AWSRule
+from reflex_core import AWSRule, subscription_confirmation
 
 
 class CloudWatchLogsUnencrypted(AWSRule):
@@ -44,5 +44,9 @@ class CloudWatchLogsUnencrypted(AWSRule):
 
 def lambda_handler(event, _):
     """ Handles the incoming event """
+    print(event)
+    if subscription_confirmation.is_subscription_confirmation(event):
+        subscription_confirmation.confirm_subscription(event)
+        return
     rule = CloudWatchLogsUnencrypted(json.loads(event["Records"][0]["body"]))
     rule.run_compliance_rule()
